5f035a499f9580ca1bfe95f8a9801c900f47f762
anhtrung123A <phamtrung.tla@gmail.com>
2025-09-22 21:33:36 +0700

    feat: adds follow associations and add feed to user's profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :profiles, controller: "users", only: [ :show, :edit, :update ]
+  resources :follows, only: [ :create, :destroy ]
+  resources :profiles, controller: "users", only: [ :show, :edit, :update ] do
+    resource :follow, only: [ :create, :destroy ]
+  end
   resources :posts do
     resource :like, only: [ :create, :destroy ]
   end
-
   devise_for :users
   root "home#index"
 end
